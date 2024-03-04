e393417e0042ebde73679999af1b576d2239834f
krissidibe <assowlove@gmail.com>
2024-03-04 03:22:51 +0000

    4 Mars2

--- src/app/api/factureimg/route.ts
+++ src/app/api/factureimg/route.ts
@@ -78,12 +78,12 @@ export async function POST(req:NextRequest,res:NextResponse) {
       .replaceAll("-", " ")
      
       
-      const logo =`${process.env.BASE_API_URL}/files/logo-${dataNew.enterprise.id}.png`
-      const signature =`${process.env.BASE_API_URL}/files/signature-${dataNew.enterprise.id}.png`  
+      /* const logo =`${process.env.BASE_API_URL}/files/logo-${dataNew.enterprise.id}.png`
+      const signature =`${process.env.BASE_API_URL}/files/signature-${dataNew.enterprise.id}.png`   */
       
      
-    /*   const logo =`https://paymefinance.com/files/logo-${dataNew.enterprise.id}.png`
-      const signature =`https://paymefinance.com/files/signature-${dataNew.enterprise.id}.png` */
+      const logo =`https://paymefinance.com/files/logo-${dataNew.enterprise.id}.png`
+      const signature =`https://paymefinance.com/files/signature-${dataNew.enterprise.id}.png`
       
  
       let  invoiceTypeName = "";
