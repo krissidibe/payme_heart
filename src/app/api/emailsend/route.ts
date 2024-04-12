7bda5de956546fbf1dbd77fc53e9a24fcc1c582b
krissidibe <assowlove@gmail.com>
2024-04-12 20:07:58 +0000

    12 Avril 101

--- src/app/api/emailsend/route.ts
+++ src/app/api/emailsend/route.ts
@@ -64,7 +64,7 @@ export async function POST(req:NextRequest,res:NextResponse) {
 
       
       const options = {
-        from: 'support@paymefinance.com',
+        from: 'Payme finance <support@paymefinance.com>',
         to:dataInfo.email!.toString(),
         subject: `Confirmation de Réinitialisation de Votre Mot de Passe`,
         html: emailHtml,
