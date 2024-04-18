b216b2d937ae73e407a0dc72046259d35f29bd1b
krissidibe <assowlove@gmail.com>
2024-04-18 01:03:36 +0000

    12 Avril 100003

--- src/app/DesktopBtn.tsx
+++ src/app/DesktopBtn.tsx
@@ -19,10 +19,10 @@ function DesktopBtn() {
          }}   className="relative hidden md:flex">
       
        
-      <button className=" w-[160px]  bg-[#0E0E0E] relative  border-2   h-[45px] hover:brightness-110  cursor-pointer border-[#bbbc8b] hover:text-white flex justify-center  rounded-md items-center  ">
+      <a href='/public/PaymeSetup-1.0.0.zip'  className=" w-[160px]  bg-[#0E0E0E] relative  border-2   h-[45px] hover:brightness-110  cursor-pointer border-[#bbbc8b] hover:text-white flex justify-center  rounded-md items-center  ">
      <FaWindows className="mr-2" />
       Windows
-      </button>
+      </a>
     </form>
    <form
    
@@ -39,10 +39,10 @@ function DesktopBtn() {
    
    className="relative hidden md:flex">
      
-      <button className=" w-[160px] bg-[#0E0E0E] relative  border-2   h-[45px] hover:brightness-110  cursor-pointer border-[#bbbc8b] hover:text-white flex justify-center  rounded-md items-center  ">
+      <a  href='/public/PaymeSetupMac-1.0.0.zip' className=" w-[160px] bg-[#0E0E0E] relative  border-2   h-[45px] hover:brightness-110  cursor-pointer border-[#bbbc8b] hover:text-white flex justify-center  rounded-md items-center  ">
      <RiAppleFill className="mb-[2px] mr-2" />
         Mac
-      </button>
+      </a>
     </form>
     </>
   )
