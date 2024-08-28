d9c09e64e55e0497e517d57f030424eebeb7579e
krissidibe <assowlove@gmail.com>
2024-08-28 17:48:34 +0000

    28 08 3

--- lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from 'stripe';
-
-
-export const stripe  = new Stripe(process.env.NODE_ENV === "development" ?  "sk_test_V8VT1XR93HFl0cFzKckPD45v" : "",  {
-    apiVersion:"2024-06-20",
-    typescript:true
-})
