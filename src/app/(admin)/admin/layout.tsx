242cce24c192eabd59655292168d31869826271b
krissidibe <assowlove@gmail.com>
2024-11-07 17:36:24 +0000

    fix 13 Mobile

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,37 +1,25 @@
-import '../../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Sidebar from './_components/Sidebar'
-import { Toaster } from "@/components/ui/sonner" 
-const inter = Inter({ subsets: ['latin'] })
+import "../../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Sidebar from "./_components/Sidebar";
+import { Toaster } from "@/components/ui/sonner";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Payme Admin',
-  
-}
+  title: "Payme Admin",
+};
 
 export default async function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-   
   return (
-
-   
-<div  className="w-screen min-w-full  h-screen flex min-h-screen bg-[#060606] text-white">
-   {/*  */}
-  <Sidebar/>  
-  <Toaster />
-        <main className='w-full p-10 '>
-
-        {children}
-        </main>
-        </div>
-
-  
-    
-      
-   
-  )
+    <div className="w-screen min-w-full overflow-scroll   h-screen flex min-h-screen bg-[#060606] text-white">
+      {/*  */}
+      <Sidebar />
+      <Toaster />
+      <main className="w-full p-10 ">{children}</main>
+    </div>
+  );
 }
