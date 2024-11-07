e28eceebd2047ac3177858f0e0079ba19bdd312a
krissidibe <assowlove@gmail.com>
2024-11-07 07:17:44 +0000

    fix 10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   
   return (
     <html lang="fr" className='!scroll-smooth no-scrollbar w-full h-full no-scrollbar' >
-
+{/*  <meta  name="viewport" content= 
+            "width=device-width, user-scalable=no" /> */}
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  
       <body className={inter.className}>
       <TooltipProvider>
         {children}
